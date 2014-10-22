f4aa5faf85f0dbfb16b4b4a730b948cc1b240333
Martin Halamíček <martin@halamicek.cz>
2014-10-22 18:24:46 +0200

    logging: don't use logrotate

--- recipes/logging.rb
+++ recipes/logging.rb
@@ -3,3 +3,7 @@ include_recipe 'papertrail-rsyslog'
 
 rsyslog_default_template = resources(:template => "/etc/rsyslog.d/50-default.conf")
 rsyslog_default_template.cookbook "keboola-common"#
+
+file "/etc/cron.daily/logrotate" do
+  action :delete
+end
